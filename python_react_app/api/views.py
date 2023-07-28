cb46687f6463e3f9aa15d03a0c7a8f2c0350ea5c
Aishwarya-yerram <yerramaishwarya9@gmail.com>
2023-07-28 09:21:07 -0400

    added the views using the mixins

--- python_react_app/api/views.py
+++ python_react_app/api/views.py
@@ -6,8 +6,40 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import APIView
+from rest_framework import generics
+from rest_framework import mixins
 
 
+class ArticleList(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+
+	queryset = Article.objects.all()
+	serializer_class = ArticleSerializer
+
+	def get(self, request):
+		return self.list(request)
+
+	def post(self, request):
+		return self.create(request)
+
+class ArticleDetails(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin,
+					 mixins.DestroyModelMixin):
+
+	queryset = Article.objects.all()
+	serializer_class = ArticleSerializer
+
+	lookup_field = 'id'
+
+	def get(self, request, id):
+		return self.retrieve(request, id=id)
+
+	def put(self, request, id):
+		return self.update(request, id=id)
+
+	def delete(self, request, id):
+		return self.destroy(request, id=id)
+
+'''
+# these are class based Api view
 class ArticleList(APIView):
 
 	def get(self, request):
@@ -51,7 +83,7 @@ class ArticleDetails(APIView):
 		article = self.get_object(id)
 		article.delete()
 		return Response(status=status.HTTP_204_NO_CONTENT)
-
+	'''
 
 # Create your views here.
 
