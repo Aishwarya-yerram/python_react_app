9f7edc83d0067fc50381f248f282d8ea4d91931f
Aishwarya-yerram <yerramaishwarya9@gmail.com>
2023-08-08 01:39:52 -0400

    fetching the data from backend in the react app and displaying

--- python_react_app/react-app/src/App.js
+++ python_react_app/react-app/src/App.js
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import Name from './components/Name';
-import Example from './components/Example';
-import Form from './components/Form';
+// import Name from './components/Name';
+// import Example from './components/Example';
+// import Form from './components/Form';
+import {useState, useEffect} from 'react';
 
 function App() {
+  
+  const [articles, setArticles] = useState([])
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/articles/', {
+      'method': 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token 9ea43624a5a7a47f068cd29d3ae000271efac67d' 
+      }
+    })
+    .then(resp => resp.json())
+    .then(resp => setArticles(resp))
+    .catch(error => console.log(error))
+  }, [])
   
   function clicked() {
     alert('class button is clicked')
@@ -12,10 +28,16 @@ function App() {
 
 
   return (
-    <div className="container">
-    <Name />
-    <Example names={['ruby', 'python']}/>
-    <Form />
+    <div className="App">
+      <h1>Django React app </h1>
+      <h2> Articles list </h2>
+      {articles.map(article =>{
+        return (
+          <div>
+            <h2 key={article.id}>{article.title}</h2>
+            <p> {article.description} </p>
+          </div>)
+      })}
     </div>
   );
 }
